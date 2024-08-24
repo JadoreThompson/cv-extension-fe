1b410b896985547fa29991f8f635485e3b305f42
LucioJamieAI <47329590+LucioJamieAI@users.noreply.github.com>
2024-08-24 03:56:38 +0100

    Built Demo Vesion

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     /* TODO:
-            - Save user_id in local storage, check to see if auth is necessary
+            - Add Loading screen when user generates cover letter.
             - Add phone number for registering. Let the user know that
                 the email and phone number will be used in the cover letter
                 as points of contact
@@ -18,81 +18,114 @@ document.addEventListener("DOMContentLoaded", function() {
     const loginSubmit = document.getElementById("loginSubmit");
 
     const addCVScreen = document.getElementById("addCVScreen");
-    const cvSubmit = document.querySelector("#addCVScreen .auth-container .form-group input[type='submit']");
+    const cvSubmit = document.getElementById("cvSubmit");
 
     const coverLetterScreen = document.getElementById("coverLetterScreen");
+
+    const loadScreen = document.getElementById("loadingScreen");
+
+    const footer = document.getElementById("footer");
+    footer.style.visibility = "hidden";
 
     if(localStorage.getItem("user_id")) {
         loginScreen.style.display = "none";
         coverLetterScreen.style.display = "block";
+        footer.style.visibility = "visible";
     }
+
 
     // Authentication
     signUpLink.addEventListener("click", function(){
         loginScreen.style.display = "none";
         signUpScreen.style.display = "block";
     });
-    signUpSubmit.addEventListener("click", function(){
-        const name = document.querySelector("#signUpScreen .auth-container input[type='text']").value;
-        const email = document.querySelector("#signUpScreen .auth-container input[type='email']").value;
-        const password = document.querySelector("#signUpScreen .auth-container input[type='password']").value;
+    signUpSubmit.addEventListener("click", async function(){
+        const name = document.querySelector("#signUpScreen .auth-card .card-body .form-group input[type='text']").value;
+        const email = document.querySelector("#signUpScreen .auth-card .card-body .form-group input[type='email']").value;
+        const password = document.querySelector("#signUpScreen .auth-card .card-body .form-group input[type='password']").value;
 
-        fetch("http://localhost:8000/signup", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({email: email, password: password, name: name})
-        })
-        .then(response => {
+        try {
+            const response = await fetch("http://localhost:8000/signup", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({email: email, password: password, name: name})
+            });
+
             if (!response.ok) {
-                throw new Error('Signup failed');
+                throw new Error("Error signing up");
             }
-            return response.json();
-        })
-        .then(data => {
-            console.log("Success:", data.message);
-            signUpScreen.style.display = "none";
-            addCVScreen.style.display = "block";
-        })
-        .catch(error => {
-            console.error("Error:", error);
-            // Handle error (e.g., display error message to user)
-        });
+
+             const data = await response.json();
+             console.log("Success Signing Up: ", data.detail);
+
+             showAlert("Successfully sign up");
+             signUpScreen.style.display = "none";
+             coverLetterScreen.style.display = "block";
+             footer.style.visibility = "visible";
+        } catch (error) {
+            console.log("Error");
+        }
     });
 
     loginLink.addEventListener("click", function(){
         signUpScreen.style.display = "none";
         loginScreen.style.display = "block";
     });
-    loginSubmit.addEventListener("click", function(){
-        const email = document.querySelector("#loginScreen .auth-container input[type='email']").value;
-        const password = document.querySelector("#loginScreen .auth-container input[type='password']").value;
+    loginSubmit.addEventListener("click", async function(){
+        const email = document.querySelector("#loginScreen .auth-card .card-body input[type='email']").value;
+        const password = document.querySelector("#loginScreen .auth-card .card-body input[type='password']").value;
 
-        fetch("http://localhost:8000/login", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({email: email, password: password})
-        })
-        .then(response => {
+        try {
+            const response = await fetch("http://localhost:8000/login", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({email: email, password: password})
+            });
+
             if (!response.ok) {
-                throw new Error("Signup Failed");
+                throw new Error("Error logging in");
             }
-            return response.json()
-        })
-        .then(data => {
+
+            const data = await response.json();
+            console.log("Success logging in");
             localStorage.setItem("user_id", data.user_id);
-            console.log("Success: ", data.user_id);
+            showAlert("Successfully logged In")
+
             loginScreen.style.display = "none";
-            addCVScreen.style.display = "block";
-        })
-        .catch(error => {
-            console.error("Error: ", error);
-        });
+            coverLetterScreen.style.display = "block";
+            footer.style.visibility = "visible";
+        } catch (error) {
+            console.log("Had to catch an error");
+        }
     });
 
     // Saving CV
-    // TODO: handle the CV being added
-    cvSubmit.addEventListener("click", function(){
-        const fileInput = document.querySelector("#addCVScreen .auth-container .form-group input[type='file']");
+    if (localStorage.getItem("cv")) {
+        const cv = localStorage.getItem("cv");
+        console.log("CV: ", cv);
+        const fileDisplayName = document.querySelector(".inner-container h4");
+        fileDisplayName.textContent = cv;
+    }
+
+    const submitter = document.getElementById("cvSubmitter");
+    submitter.addEventListener("click", function() {
+        const fileInput = document.querySelector(".cv-submit").click();
+    });
+
+    const cvInputBox = document.querySelector(".cv-submit");
+    cvInputBox.addEventListener("change", function(event){
+        const file = event.target.files[0];
+        if (file) {
+            const fileDisplayName = document.querySelector(".inner-container h4");
+            fileDisplayName.textContent = file.name;
+        }
+    });
+
+    cvSubmit.addEventListener("click", async function(){
+        addCVScreen.style.display = "none";
+        loadingScreen.style.display = "flex";
+
+        const fileInput = document.querySelector(".cv-submit");
         const cv = fileInput.files[0];
         const userId = 1;
 
@@ -100,25 +133,48 @@ document.addEventListener("DOMContentLoaded", function() {
         formData.append("cv", cv);
         formData.append("user_id", userId);
 
-        fetch("http://localhost:8000/upload-cv", {
-            method: "POST",
-            body: formData
-        })
-        .then(response => {
+        try {
+            const response = await fetch("http://localhost:8000/upload-cv", {
+                method: "POST",
+                body: formData
+            });
+
             if (!response.ok) {
-                throw new Error("Error");
-            } return response.json()
-        })
-        .then(data => {
-            addCVScreen.style.display = "none";
-            coverLetterScreen.style.display = "block";
-        });
+                showAlert("Error saving CV");
+            }
+
+            const data = await response.json();
+            localStorage.setItem("cv", cv.name);
+            showAlert("CV Saved");
+
+        } catch (error) {
+            showAlert("Error saving CV");
+        } finally {
+            loadScreen.style.display = "none";
+            addCVScreen.style.display = "block";
+        }
     });
 
     // Generating Cover Letter
-    // TODO: Generate the response from the necessary API calls for generation
-    const jobDescSubmit = document.querySelector(".job-description input[type='submit']");
-    jobDescSubmit.addEventListener("click", async function(event) {
+    const jobDescBox = document.getElementById("jobDescBox");
+    jobDescBox.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            document.getElementById("jobDescSubmit").click();
+        }
+    });
+
+    const jobDescSubmit = document.getElementById("jobDescSubmit");
+    let originalColor = jobDescSubmit.style.color;
+    jobDescSubmit.addEventListener("mousedown", function() {
+        jobDescSubmit.style.color = "blue";
+    });
+
+    jobDescSubmit.addEventListener("mouseup", function() {
+        jobDescSubmit.style.color = originalColor;
+    });
+
+    jobDescSubmit.addEventListener("click", async function() {
         event.preventDefault(); // Prevent form submission
 
         const jobDesc = document.getElementById("jobDescBox").value;
@@ -133,32 +189,105 @@ document.addEventListener("DOMContentLoaded", function() {
                 });
 
                 if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
+                    await showAlert("failed");
+                    console.log("Monkey");
                 }
 
                 const data = await response.json();
                 console.log("Success: ", data.cover_letter);
 
                 // TODO: Handle the response data (e.g., display the generated cover letter)
-                const container = document.querySelector(".cover-container ul");
+                const container = document.querySelector(".card-body ul");
                 const li = document.createElement("li");
-                const div = document.createElement("div")
-                div.classList.add("cover-letter");
-                const p = document.createElement("p");
+                const div = document.createElement("div");
+                const span = document.createElement("span");
+                const span2 = document.createElement("span");
 
-                p.textContent = data.cover_letter;
-                div.appendChild(p);
+                span.classList.add("copy-button");
+                div.classList.add("cover-letter-container");
+
+                span2.textContent = "Copy";
+                span.appendChild(span2);
+                div.appendChild(span);
+                const paragraphs = data.cover_letter.split("\n\n");
+                paragraphs.forEach(paragraph => {
+                    const p = document.createElement("p");
+                    p.textContent = paragraph.trim();
+                    div.appendChild(p);
+                });
+
                 li.appendChild(div);
                 container.appendChild(li);
 
             } catch (error) {
                 console.error("Error:", error);
+                showAlert("Failed to generate application");
                 // TODO: Handle errors (e.g., display error message to user)
             }
         } else {
-            console.error("Job description is empty");
-            // TODO: Inform the user that job description is required
+            showAlert("Job description is empty");
+            // TODO: Inform the user that job description is required and they need to be logged in
         }
     });
 
+    const copyButton = document.querySelector(".copy-button");
+    originalColor = copyButton.style.backgroundColor;
+    copyButton.addEventListener("mousedown", function() {
+        copyButton.style.backgroundColor = "#fdfbf7";
+    });
+    copyButton.addEventListener("mouseup", function() {
+        copyButton.style.backgroundColor = originalColor;
+    });
+
+    copyButton.addEventListener("click", function() {
+        const div = this.closest(".cover-letter-container");
+        const p = div.querySelector('p');
+        navigator.clipboard.writeText(p.textContent);
+        showAlert("Copied CV!");
+    });
+
+    // Extension Alert
+    function showAlert(message) {
+        const alertBox = document.getElementById('customAlert');
+        const alertMessage = document.getElementById('alertMessage');
+        alertMessage.textContent = message; // Set the alert message
+        alertBox.classList.remove('inactive');
+        alertBox.classList.add('active');
+
+        // Auto-close after 3 seconds
+        setTimeout(() => {
+            alertBox.classList.add('inactive');
+            alertBox.classList.remove('active');
+        }, 3000);
+    }
+
+    // Footer
+    const icons = document.querySelectorAll('#footer .icon-container i');
+    console.log("icons: ", icons);
+
+    function handleMouseDown(event) {
+        event.target.style.color = '#6db2fd';
+    }
+
+    function handleMouseUp(event) {
+        event.target.style.color = '';
+    }
+
+    icons.forEach(icon => {
+        icon.addEventListener('mousedown', handleMouseDown);
+        icon.addEventListener('mouseup', handleMouseUp);
+    });
+
+    const addCVIcon = document.getElementById("addCVIcon");
+    const getCoverLetterIcon = document.getElementById("getCoverLetterIcon");
+
+    addCVIcon.addEventListener("click", function() {
+        coverLetterScreen.style.display = "none";
+        addCVScreen.style.display = "block";
+    });
+
+    getCoverLetterIcon.addEventListener("click", function(){
+        addCVScreen.style.display = "none";
+        coverLetterScreen.style.display = "block";
+    });
 });
